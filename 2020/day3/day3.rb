2b374010fe15b3f32b4645f904d68a533ea674b8
Alejandro Bezdjian <alebezdjian@gmail.com>
2020-12-03 11:45:44 -0300

    Added 2020 day 3

---
+++ 2020/day3/day3.rb
@@ -0,0 +1,26 @@
+map = File.read('./map.txt').split("\n")
+
+RIGHT = 3
+# DOWN = 1
+
+TREE = '#'
+SPACE = '.'
+
+# current_y = 0
+current_x = 0
+
+trees = 0
+spaces = 0
+
+# Skip the first line and go through each line (we don't need the Y movement for now since it's 1)
+map[1..-1].each do |line|
+  current_x = (current_x + RIGHT) % line.size
+
+  case line[current_x]
+    when TREE then trees += 1
+    when SPACE then spaces += 1
+  end
+end
+
+puts "Spaces seen: #{spaces}"
+puts " Trees seen: #{trees}"
