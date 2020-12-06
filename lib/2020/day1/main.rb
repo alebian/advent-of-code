16510051245392ad0f63785dad194a416130ac17
Alejandro Bezdjian <alebezdjian@gmail.com>
2020-12-06 12:56:40 -0300

    Finished 2020 day 1

--- lib/2020/day1/main.rb
+++ lib/2020/day1/main.rb
@@ -18,3 +18,16 @@ end
 
 puts result.inspect
 puts result[0] * result[1]
+
+# TODO: improve this naive implementation
+INPUT.each_with_index do |input1, i|
+  INPUT.each_with_index do |input2, j|
+    INPUT.each_with_index do |input3, k|
+      if i != j && i != k && j != k
+        if (input1 + input2 + input3) == TARGET
+          puts (input1 * input2 * input3)
+        end
+      end
+    end
+  end
+end
