375d9153fb79ba559ad73845e70b1a42438a061b
Alejandro Bezdjian <alebezdjian@gmail.com>
2020-12-06 12:49:29 -0300

    Finished 2020 day 3

--- lib/2020/day3/main.rb
+++ lib/2020/day3/main.rb
@@ -2,27 +2,34 @@ require_relative '../../helpers'
 
 INPUT = Helpers.read_input(2020, 3)
 
-RIGHT = 3
-# DOWN = 1
-
 TREE = '#'
 SPACE = '.'
 
-# current_y = 0
-current_x = 0
+def find_trees(rows, slope_x, slope_y)
+  current_x = 0
+  current_y = 0
 
-trees = 0
-spaces = 0
+  trees = 0
+  spaces = 0
 
-# Skip the first line and go through each line (we don't need the Y movement for now since it's 1)
-INPUT[1..-1].each do |line|
-  current_x = (current_x + RIGHT) % line.size
+  while current_y < rows.size
+    line = rows[current_y]
+    case line[current_x]
+      when TREE then trees += 1
+      when SPACE then spaces += 1
+    end
 
-  case line[current_x]
-    when TREE then trees += 1
-    when SPACE then spaces += 1
+    current_x = (current_x + slope_x) % line.size
+    current_y += slope_y
   end
+
+  return trees
 end
 
-puts "Spaces seen: #{spaces}"
-puts "Trees seen: #{trees}"
+puts [
+    find_trees(INPUT, 1, 1),
+    find_trees(INPUT, 3, 1),
+    find_trees(INPUT, 5, 1),
+    find_trees(INPUT, 7, 1),
+    find_trees(INPUT, 1, 2)
+].inject(&:*)
